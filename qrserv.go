3491f75e4e95fdfced8dc244e60af0161fe85ec6
Chris Harrington <ironiridis@gmail.com>
2015-12-15 16:27:46 -0600

    initial code, probably violating license wrt MIT & BSD

---
+++ qrserv.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"bytes"
+	"encoding/base64"
+
+	"github.com/gin-gonic/gin"
+	"github.com/ironiridis/qrserv/vendor/rsc/qr"
+)
+
+// PNGRequest renders a PNG file
+func PNGRequest(c *gin.Context) {
+	c.Header("X-Attribution", "Uses code.google.com/p/rsc/qr")
+	qrc, err := qr.Encode(c.Param("URL"), qr.L)
+	if err != nil {
+		c.AbortWithError(500, err)
+		return
+	}
+	// TODO: caching directives
+	c.Data(200, "image/png", qrc.PNG())
+}
+
+// HTMLRequest renders an HTML document with an embedded PNG <img>
+func HTMLRequest(c *gin.Context) {
+	c.Header("X-Attribution", "Uses code.google.com/p/rsc/qr")
+	qrc, err := qr.Encode(c.Param("URL"), qr.L)
+	if err != nil {
+		c.AbortWithError(500, err)
+		return
+	}
+	// TODO: caching directives
+	var out bytes.Buffer
+	out.Write([]byte("<img src='data:image/png;base64,"))
+	pngenc := base64.NewEncoder(base64.StdEncoding, &out)
+	pngenc.Write(qrc.PNG())
+	pngenc.Close()
+	out.Write([]byte("'>"))
+	c.Data(200, "text/html", out.Bytes())
+}
+
+func main() {
+	r := gin.Default()
+	r.GET("/png/:URL", PNGRequest)
+	r.GET("/html/:URL", HTMLRequest)
+	r.Run("127.0.0.1:8080")
+}
