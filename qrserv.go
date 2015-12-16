63aad8382be70aafbec642a8a2a54150f115247e
Chris Harrington <ironiridis@gmail.com>
2015-12-16 00:59:28 -0600

    caching

--- qrserv.go
+++ qrserv.go
@@ -3,10 +3,15 @@ package main
 import (
 	"bytes"
 	"encoding/base64"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/ironiridis/qrserv/vendor/rsc/qr"
 )
+
+// per RFC2616 sec 14.21: 1 year in the future is the maximum cache expiry
+// pre-calculate this once at startup to avoid pointless CPU churn
+var futureExpire = time.Now().AddDate(1, 0, 0).Format(time.RFC1123)
 
 func encodableString(c *gin.Context) string {
 	// note that we tried to use c.Request.URL.Path and c.Request.URL.RawPath
@@ -19,27 +24,31 @@ func encodableString(c *gin.Context) string {
 	return c.Param("URL")[1:]
 }
 
+func standardHeaders(c *gin.Context) {
+	c.Header("X-Attribution", "Uses code.google.com/p/rsc/qr")
+	c.Header("Cache-Control", "max-age=31536000") // 60*60*24*365 aka 1 year
+	c.Header("Expires", futureExpire)
+}
+
 // PNGRequest renders a PNG file
 func PNGRequest(c *gin.Context) {
-	c.Header("X-Attribution", "Uses code.google.com/p/rsc/qr")
 	qrc, err := qr.Encode(encodableString(c), qr.L)
 	if err != nil {
 		c.AbortWithError(500, err)
 		return
 	}
-	// TODO: caching directives
+	standardHeaders(c)
 	c.Data(200, "image/png", qrc.PNG())
 }
 
 // HTMLRequest renders an HTML document with an embedded PNG <img>
 func HTMLRequest(c *gin.Context) {
-	c.Header("X-Attribution", "Uses code.google.com/p/rsc/qr")
 	qrc, err := qr.Encode(encodableString(c), qr.L)
 	if err != nil {
 		c.AbortWithError(500, err)
 		return
 	}
-	// TODO: caching directives
+	standardHeaders(c)
 	var out bytes.Buffer
 	out.Write([]byte("<img src='data:image/png;base64,"))
 	pngenc := base64.NewEncoder(base64.StdEncoding, &out)
